20285f1de393ee0410a57c724aa0cd3e92206470
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-11 10:26:05 +0900

    add: LoginViewController

---
+++ SeSACTesting/Presentation/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  SeSACTesting
+//
+//  Created by Taekwon Lee on 12/11/23.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
