865d42005bdd8c7c722d6ab242151cffc9d289fd
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-12 10:20:20 +0900

    add: README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# SeSACTesting
+> CI/CD & DI 수업
+
+## Unit Test
+
+## DI (Dependency Injection)
+
---
+++ SeSACTesting/DI/DI.swift
@@ -0,0 +1,8 @@
+//
+//  DI.swift
+//  SeSACTesting
+//
+//  Created by Taekwon Lee on 12/12/23.
+//
+
+import Foundation
