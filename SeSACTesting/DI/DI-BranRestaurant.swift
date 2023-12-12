d039a8673891461af62ca77bbc0b8c4232dbec1c
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-12 12:19:20 +0900

    docs: DI & DIP

--- SeSACTesting/DI/DI-BranRestaurant.swift
+++ SeSACTesting/DI/DI-BranRestaurant.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-/*
- Bran -> Bran's Restaurant
- Hue -> trying to eat food in Bran's
- 
- - 🔗 의존 관계
-   - 휴는 상위 모듈, 브랜반점은 하위 모듈 (휴님이 브랜반점에 의존함)
-     - 🔥 하위 모듈 변경 시 상위 모듈에 계속 영향을 준다
-   - Hue -> BranRestaurant -> Bran
-     - 🔥 브랜반점의 코드 변화가 휴님에게 곧바로 영향을 미친다
-     - 🔥 하위 모듈의 코드 변화가 상위 모듈에 영향을 미친다
- - ❓ 이를 어떻게 해결해야 할까?
- - ✅ 프로토콜에 의해 해결
- -> 각각의 레스토랑이 레스토랑 프로토콜을 준수하도록
- -> 각각의 주방장이 주방장 프로토콜을 준수하도록
- -> 구현체가 아닌 추상화에 의존하도록 변경하기
- */
-
 //MARK: - ⭐️ (0) Protocol
 protocol HamburgerRestaurant {
     func makeLunchMenu() -> String
@@ -110,13 +93,6 @@ final class KokoRestaurant: BunsikRestaurant {
 }
 
 //MARK: - (3) Customer
-/*
- - 휴는 상위 모듈, 브랜반점은 하위 모듈 (휴님이 브랜반점에 의존함)
- - 의존 관계
-   - Hue -> BranRestaurant -> Bran
- 
- - ❓ 상위 모듈이 하위 모듈의 변화를 모르고 코드만 가져올 수는 없을까?
- */
 final class Hue: Customer {
     
     private var hamburgerRestaurant: HamburgerRestaurant
@@ -139,7 +115,69 @@ final class Hue: Customer {
     }
 }
 
+//MARK: - (4) 의존성 주입 (Dependency Injection)
+let bran = Bran()
+let branRestaurant = BranRestaurant(owner: bran)
+
+let koko = Koko()
+let kokoRestaurant = KokoRestaurant(owner: koko)
+
 let hue = Hue(
-    hamburgerRestaurant: BranRestaurant(owner: Bran()),
-    bunsikRestaurant: KokoRestaurant(owner: Koko())
+    hamburgerRestaurant: branRestaurant,
+    bunsikRestaurant: kokoRestaurant
 )
+
+//MARK: - (A) 의존 관계 문제 발생과 해결책
+/*
+ Bran -> Bran's Restaurant
+ Hue -> trying to eat food in Bran's
+ 
+ - 🔗 (1) 문제 발생 of 의존성
+   - 휴는 상위 모듈, 브랜반점은 하위 모듈 (휴님이 브랜반점에 의존함)
+     - 🔥 하위 모듈 변경 시 상위 모듈에 계속 영향을 준다
+   - Hue --> BranRestaurant --> Bran
+     - 🔥 브랜반점의 코드 변화가 휴님에게 곧바로 영향을 미친다
+     - 🔥 하위 모듈의 코드 변화가 상위 모듈에 영향을 미친다
+ - ❓ 이를 어떻게 해결해야 할까?
+ - 🔗 (2) 문제 해결 by 프로토콜
+   --> 각각의 레스토랑이 레스토랑 프로토콜을 준수하도록
+   --> 각각의 주방장이 주방장 프로토콜을 준수하도록
+   --> 구현체가 아닌 추상화에 의존하도록 변경하기
+ */
+
+//MARK: - (B) 상위 모듈 & 하위 모듈
+/*
+ - 휴는 상위 모듈, 브랜반점은 하위 모듈 (휴님이 브랜반점에 의존함)
+ - 의존 관계
+   - Hue -> BranRestaurant -> Bran
+ 
+ - ❓ 상위 모듈이 하위 모듈의 변화를 모르고 코드만 가져올 수는 없을까?
+*/
+
+//MARK: - (C) 의존성 분리
+/*
+ - ✅ 브랜 & 코코의 생성 시점과 사용을 분리한다
+ - ⭐️ 객체의 생성과 사용의 관심을 분리한다 -> 생성자를 통해서 의존성을 주입한다
+   --> 구현체가 아닌 인터페이스(추상화)에 의존한다
+ */
+
+//MARK: - (D) 의존성 주입 (Dependency Injection)
+/*
+    let bran = Bran()
+    let branRestaurant = BranRestaurant(owner: bran)
+
+    let koko = Koko()
+    let kokoRestaurant = KokoRestaurant(owner: koko)
+
+    let hue = Hue(
+        hamburgerRestaurant: branRestaurant,
+        bunsikRestaurant: kokoRestaurant
+    )
+ */
+
+//MARK: - (E) DIP vs DI == 원칙 vs 수단
+/*
+ 1. DI (Depenency Injection; 의존성 주입)
+ 2. DIP (Dependency Inversion Principle; 의존성 역전 원칙)
+ -> DIP의 구현 기법으로 DI를 사용할 수 있다
+ */
