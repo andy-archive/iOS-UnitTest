d19cfd50a34dc1ac35f29b2e06a5db9139033d7e
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-12 10:21:26 +0900

    add: DI - Dependency Injection

--- SeSACTesting/DI/DI.swift
+++ SeSACTesting/DI/DI.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+/*
+ Bran -> Bran's Restaurant
+ Hue -> trying to eat food in Bran's
+ */
+
+class BranRestaurant {
+    
+    func makeSignatureHamburger() -> String {
+        return "Signature Hamburger"
+    }
+    
+    func makeFrenchFries() -> String {
+        return "FrenchFries"
+    }
+    
+    func makeSoftDrink() -> String {
+        return "Soft Drink"
+    }
+}
