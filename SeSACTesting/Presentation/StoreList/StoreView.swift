bb8f415f1035490c979d0e1a79e31f3b0ebe0555
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-12-18 12:14:44 +0900

    feat: ProductView since iOS 17.0

--- SeSACTesting/Presentation/StoreList/StoreView.swift
+++ SeSACTesting/Presentation/StoreList/StoreView.swift
@@ -5,6 +5,7 @@
 //  Created by Taekwon Lee on 12/18/23.
 //
 
+import StoreKit
 import SwiftUI
 
 struct StoreView: View {
@@ -14,20 +15,29 @@ struct StoreView: View {
     
     //MARK: - UI
     var body: some View {
-        HStack {
-            Text("Remove Ad")
-            Spacer()
-            Button("$99.99") {
-                print("Button")
+        VStack {
+            HStack {
+                Text("Remove Ad")
+                Spacer()
+                Button("$99.99") {
+                    print("Button")
+                }
+            }
+            /// ProductView & SubscriptionStoreView (iOS 17.0+)
+            ForEach(shop.allProducts, id: \.id) { item in
+                ProductView(item) /// iOS 17.0
+                    .productViewStyle(.regular) // .compact
+                //SubscriptionStoreView()
             }
         }
         .padding()
         .task {
-            await shop.fetchAllProducts()
+            await shop.fetchAllProducts() /// 2, 3, 4
         }
     }
 }
 
+//MARK: - Preview
 #Preview {
     StoreView()
 }
